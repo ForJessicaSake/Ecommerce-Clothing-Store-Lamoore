47983a31fa66fe48c19dbbb47df6a754d87c437a
jessica joseph <jessicajoseph2019@yahoo.com>
2022-08-20 00:36:59 +0100

    fix:Added the cart totalprice logic

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,32 +3,32 @@ import { StateContext } from '../App';
 import { Link } from 'react-router-dom';
 
 function Cart() {
-  const { cart, removeCart} = useContext(StateContext);
+  const { cart, removeCart } = useContext(StateContext);
 
   return (
     <main className="cart-wrapper">
       <h6 className="subtitle">Home/Women/<span style={{ color: '#161616' }}>Accessories</span></h6>
       {cart && cart.map((cart) => (
         <section key={cart.id}>
-        <header className='cart-item' >
-          <img src={cart.image} alt='clothes'/>
-          <section className='cart-content'>
-            <h4>{cart.title}</h4>
-            <div className='size'>
-              <h5>Size: 6-16</h5>
-            </div>
-          </section>
-          <section className='exit'>
-            <h3 onClick ={()=>{removeCart(cart.id)}} style={{cursor:'pointer'}}>X</h3>
-            <h5>Price: ${cart.price}</h5>
-          </section>
-        </header>
-        <hr/>
-       
+          <header className='cart-item' >
+            <img src={cart.image} alt='clothes' />
+            <section className='cart-content'>
+              <h4>{cart.title}</h4>
+              <div className='size'>
+                <h5>Size: 6-16</h5>
+              </div>
+            </section>
+            <section className='exit'>
+              <h3 onClick={() => { removeCart(cart.id) }} style={{ cursor: 'pointer' }}>X</h3>
+              <h5>Price: ${cart.price}</h5>
+            </section>
+          </header>
+          <hr />
+
         </section>
       ))}
       <p className='fee'>Shopping Fee: $10</p>
-      {/* <h3 className='fee'>Total: {totalPrice}</h3> */}
+      <h3 className='fee'>Total: ${cart.reduce((total, cart) => {return cart.price + total}, 0)}</h3>
       <button className='cart-btn'><Link to='/checkout'>Checkout</Link></button>
     </main>)
 }
